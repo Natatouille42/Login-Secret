cb628802c0045e769c1f58e85664d942129cf083
neolinux38 <spam15@neolinux.fr>
2022-06-19 14:57:18 +0200

    Correction Ex2

--- f_calcul.php
+++ f_calcul.php
@@ -2,33 +2,42 @@
 
 
 ###Comptabilise les articles ajoutés au panier et crée un tableau stocké dans la variable $_SESSION###
-function Panier(){
-    if (isset($_GET["article"])) {
-        $article = $_GET["article"];
-        if (!isset($_SESSION[$article])) {
-            $_SESSION[$article] = 1;
+function AjouterArticlePanier($article){
+
+
+        if (!isset($_SESSION["panier"][$article])) {
+            $_SESSION["panier"][$article] = 1;
         } else {
-            $_SESSION[$article]++;
+            $_SESSION["panier"][$article]++;
+
         }
-    }
+
 }
 
 ###Récupère les données stockées dans la variable $_SESSION avec la fonction panier et calcule le tarif par type d'article###
 function CalculPrix($tarif){
-    foreach ($tarif as $legume => $prix){
-        if (isset ($_SESSION[$legume])){
-            $prix=round ($prix,2);
-            $article=($_SESSION[$legume]);
-            $resultat[$legume]["quantité"]=$article;        //Compabilise la quantité de chaque article pour calcul et affichage (tableau)
-            $resultat[$legume]["somme"]=$article*$prix;     //Crée un tableau en résultat pour calcul du total et affichage
-        }
-    }return $resultat;
+
+    if(isset($_SESSION["panier"])){
+      foreach ($_SESSION["panier"] as $nom_legume => $quantite){
+        $prix=round($tarif[$nom_legume],2);
+        $resultat[$nom_legume]["quantité"]=$quantite;
+        $resultat[$nom_legume]["somme"]=$prix*$quantite;
+      }
+      return $resultat;
+    }else{
+      return false;
+    }
+
+
 }
 
 ###Récupère le tableau de prix fourni par la fonction calculprix et additionne pour obtenir le total pour affichage###
 function CalculTotal($Checkout){
     $total=0;
-    foreach ($Checkout as $key =>$value){
+    if($Checkout!=false){
+      foreach ($Checkout as $key =>$value){
         $total=$total+$value["somme"];
-    }return $total;
+      }return $total;
+    }
+
 }
--- f_navigation.php
+++ f_navigation.php
@@ -1,41 +1,48 @@
 <?php
+echo "<pre>";
 print_r($_SESSION);
+echo "</pre>";
 
 ###Fonction globale de la page index, comprenant traitement et affichage###
 function Routage($utilisateurs, $page, $tarif){
 
     ###TRAITEMENT###
 
-    if (isset ($_POST["login"]) and !isset ($_SESSION["login"])) {      //Si le formulaire est rempli et qu'aucune session n'est ouverte
+
+    #Traitement pour anonyme
+    if (!isset($_SESSION["login"])){
+
+      if (isset ($_POST["login"])) {      //Si le formulaire est rempli et qu'aucune session n'est ouverte
         LogIn($_POST["login"], $_POST["pass"], $utilisateurs);      //appelle la fonction login pour connexion et ouverture de session
-    }
-    if (isset($_GET["logout"]) and ($_GET["logout"])=="true") {     //Si le bouton de déconnexion est cliqué
+      }
+
+    #Traitement sécurisé
+    }else{
+      if (isset($_GET["logout"])) {     //Si le bouton de déconnexion est cliqué
         LogOut();       //appelle la fonction logout pour écraser la session et effacer les données collectées
+      }elseif( isset($_GET["article"])) {
+        AjouterArticlePanier($_GET["article"]);
+
+      }
     }
-    if (isset($_SESSION["login"])){     //Une fois la session ouvertecomptabilise les articles placés dans le panier
-        Panier();       //comptabilise les articles placés dans le panier
-    }
-    if (isset($_GET["page"]) and ($_GET["page"])=='index'){     //Si l'utilisateur revient à la page d'accueil
-        $Checkout=CalculPrix($tarif);       //calcule le prix de chaque article selon la quantité
-        $total=CalculTotal($Checkout);      //Et calcule le montanttotal des articles sélectionnés
-        $_SESSION["hack"]++;
-    }
+
+
+
 
 
 
     ###AFFICHAGE###
-
+    #Sécurisé
     if (isset($_SESSION["login"])){     //Si la session est ouverte (utilisateur logué)
         AfficheMenu($page);     //Affiche le menu avec les liens vers les différentes pages et le bouton de déconnexion
         AfficheLien($page, $tarif);     //Et affiche le contenu des liens cliqués (articles disponibles et leur tarif)
-        if (isset($_GET["page"]) and $_GET["page"]=='index'){       //Sil'utilisateur se dirige vers la page d'accueil
-            AffichePanier($Checkout, $total);       //Affichele prix par type d'article et le prix total
-        }
+    #Redirection special Hacker
     }else{
         if (isset($_GET["page"])){      //Si essai d'accéder un lien mais sans etre connecté
             Redirect("Veuillez vous connecter pour accéder au lien");       //Redirige vers le formulaire de connexion
         }
     }
+    #Anonyme
     if (!isset($_SESSION["login"])) {       //Si l'utilisateur n'est pas connecté
         AfficheFormulaire();        //Affiche le formulaire de connexion
     }
@@ -45,8 +52,14 @@ function Routage($utilisateurs, $page, $tarif){
 ###Affiche les liens vers les différentes pages, et le bouton de déconnexion###
 function AfficheMenu($pages){
     foreach ($pages as $key => $value){
-        echo "<a href='index.php?page=".$key."'>".$value['nom_lien']."</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+        if($key!=="index"){
+          echo "<a href='index.php?page=".$key."'>".$value['nom_lien']."</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+        }else{
+          echo "<a href='index.php'>".$value['nom_lien']."</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+        }
+
     }
+
     echo "<br><br>";
     echo "<a href='index.php?logout=true'>Me déconnecter</a>";
 }
@@ -58,11 +71,16 @@ function AfficheLien($pages, $tarif){
         echo $pages[$_GET["page"]]["contenu"]["titre"]."<br><br>";
         echo $pages[$_GET["page"]]["contenu"]["description"]."<br><br>";
         if ($_GET["page"]!=='index'){       //Affiche les liens produits, sauf sur la page d'accueil
+
             foreach ($pages[$_GET["page"]]["contenu"]["articles"] as $key){
                 echo "<a href='index.php?page=".$_GET["page"]."&article=".$key."'>".$key."</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
                 echo $tarif[$key]."€<br>";
             }
         }
+    }else{
+      $Checkout=CalculPrix($tarif);       //calcule le prix de chaque article selon la quantité
+      $total=CalculTotal($Checkout);      //Et calcule le montanttotal des articles sélectionnés
+      AffichePanier($Checkout, $total);       //Affichele prix par type d'article et le prix total
     }
 }
 
@@ -82,9 +100,14 @@ function AfficheFormulaire(){
 
 ###Affiche le panier à l'aide des information fournies par les fonctions calculprix et calcultotal###
 function AffichePanier($Checkout, $total){
-    foreach ($Checkout as $produit => $value){
+    if($Checkout!=false){
+      foreach ($Checkout as $produit => $value){
         echo $value["quantité"]." x ".$produit." = ".$value["somme"]."€<br>";
+      }
+      echo "<hr>";
+      echo "total: ".$total." €";
+    }else{
+      echo "Le panier est vide";
     }
-    echo "<hr>";
-    echo "total: ".$total." €";
+
 }
